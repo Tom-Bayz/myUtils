8eee4d0d63d13ea1cadc3f3a2b4863da3cf3827c
Tom-Bayz <organic.chemistry3164@gmail.com>
2021-05-06 00:02:33 +0900

    added bat files

--- weather_data/.ipynb_checkpoints/get_jwaforecast-checkpoint.bat
+++ weather_data/.ipynb_checkpoints/get_jwaforecast-checkpoint.bat
@@ -3,4 +3,4 @@ call C:\Users\organ\anaconda3\Scripts\activate.bat
 
 D:
 cd "Google Drive\workspace\myUtils\weather_data"
-call python WeatherNews_dataset.py
+call python JapanWeatherAssociation_dataset.py
--- weather_data/.ipynb_checkpoints/get_wnforecast-checkpoint.bat
+++ weather_data/.ipynb_checkpoints/get_wnforecast-checkpoint.bat
@@ -1,5 +1,6 @@
+C:
 call C:\Users\organ\anaconda3\Scripts\activate.bat
-D:
 
-cd "Google Drive\workspace\myUtils"
-call python weathernews_dataset.py
+D:
+cd "Google Drive\workspace\myUtils\weather_data"
+call python WeatherNews_dataset.py
